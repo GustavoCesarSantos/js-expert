3a2148d8f41d5c97ce057899a92fe62385e43c6c
GustavoCesarSantos <gustavocs789@gmail.com>
2023-10-12 23:29:45 -0300

    test: escreve testes do arquivo de rotas

--- semana-js-expert-05/gdrive-webapi/tests/unit/routes.test.js
+++ semana-js-expert-05/gdrive-webapi/tests/unit/routes.test.js
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "@jest/globals";
+import { describe, test, expect, jest } from "@jest/globals";
 import { Routes } from "../../src/routes";
 
 describe("Routes test suite", () => {
@@ -11,6 +11,30 @@ describe("Routes test suite", () => {
       };
       routes.setSocketInstance(ioObj);
       expect(routes.io).toStrictEqual(ioObj);
+    });
+  });
+
+  describe("handle", () => {
+    const defaultParams = {
+      request: {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        method: "",
+        body: {},
+      },
+      response: {
+        writeHead: jest.fn(),
+        setHeader: jest.fn(),
+        end: jest.fn(),
+      },
+    };
+    test("Given an inexistent route it should choose default route", () => {
+      const routes = new Routes();
+      const params = { ...defaultParams };
+      params.request.method = "inexistent";
+      routes.handle(params.request, params.response);
+      expect(params.response.end).toHaveBeenCalledWith("Hello World");
     });
   });
 });
